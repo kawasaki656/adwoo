ee03c939857605c4bbcfc331a9c082579e38f0bc
kawasaki656 <kawasaki646@gmail.com>
2017-07-17 00:41:54 +0300

    add map template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./map.css']
 })
 export class AppComponent {
   title = 'Adwoo';
