8f4852b4680a08fc10f0ca0650309e8e3c2a3ff7
kawasaki646 <kawasaki646@gmail.com>
2017-07-21 11:28:42 +0300

    add header, calculate width/height(not correct)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./map.css']
+  styleUrls: ['./map.css', './header.css', './content.css']
 })
 export class AppComponent {
   title = 'Adwoo';
+  screenHeight: number;
+  screenWidth: number;
+  constructor(el: ElementRef) {
+    this.screenWidth = document.body.clientWidth;
+    this.screenHeight = document.body.offsetHeight;
+    document.body.style.height = '870px';
+  }
 }
