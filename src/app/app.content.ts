2417e86949c1e98fb86521a5724cd07d471a70c2
kawasaki646 <kawasaki646@gmail.com>
2017-07-28 17:59:39 +0300

    cancel rotation

--- src/app/app.content.ts
+++ src/app/app.content.ts
@@ -8,10 +8,17 @@ import {Component, ElementRef, OnInit, Input} from '@angular/core';
 export class AppContent implements OnInit{
   ngOnInit(): void {
     this.fillMap();
+    this.screenWidth = document.documentElement.clientWidth;
+    this.screenHeight = document.documentElement.clientHeight;
+    this.cellHeight = this.screenHeight / 5;
+    this.cellWidth = this.screenWidth / 6;
+    //this.rotateMap = 145;
+    document.body.style.height = this.screenHeight + 'px';
+    document.body.style.width = this.screenWidth + 'px';
     let screenDom = window.getComputedStyle(document.getElementsByClassName("screen").item(0));
     this.mapHeight = screenDom.height;
     this.mapWidth = screenDom.width;
-    console.log(window.getComputedStyle(document.getElementsByClassName("map").item(0)))
+    //console.log(window.getComputedStyle(document.getElementsByClassName("map").item(0)))
   }
   sizemap:number;
   title = 'Adwoo';
@@ -24,15 +31,6 @@ export class AppContent implements OnInit{
   mapHeight: string;
   mapWidth: string;
   constructor(el: ElementRef) {
-    this.screenWidth = document.documentElement.clientWidth;
-    this.screenHeight = document.documentElement.clientHeight;
-    this.cellHeight = this.screenHeight / 5;
-    this.cellWidth = this.screenWidth / 6;
-    this.rotateMap = 145;
-    document.body.style.height = this.screenHeight + 'px';
-    document.body.style.width = this.screenWidth + 'px';
-
-    //window.getComputedStyle(el.nativeElement.querySelector("#map").nativeElement, null)
   }
   fillMap():void {
     this.sizemap = 3;
