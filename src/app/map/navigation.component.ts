e1986d535e935a564da0272fc94f74aa6d0e2b2e
kawasaki646 <kawasaki646@gmail.com>
2017-10-18 12:02:37 +0300

    navigation by click and move

--- src/app/map/navigation.component.ts
+++ src/app/map/navigation.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import { NavigationService } from "./navigation.service";
 import {ScreenService} from "../screen/screen.service";
+import {isUndefined} from "util";
 
 @Component({
   selector: 'navigation',
@@ -10,6 +11,24 @@ import {ScreenService} from "../screen/screen.service";
 export class NavigationComponent {
   screen:any;
   navigation:any;
+  lastMove: MouseEvent;
+
+  @HostListener('mousemove', ['$event'])
+  onMousemove(event: MouseEvent) {
+    if(!isUndefined(this.lastMove) && this.navigation.mouseDown) {
+      this.navigation.x += event.clientX - this.lastMove.clientX;
+      this.navigation.y += event.clientY - this.lastMove.clientY;
+    }
+    this.lastMove = event;
+  }
+  @HostListener('mouseup')
+  onMouseup() {
+    this.navigation.mouseDown = false;
+  }
+  @HostListener('mousedown')
+  onMousedown() {
+    this.navigation.mouseDown = true;
+  }
   constructor(navigation:NavigationService, screen:ScreenService) {
     this.screen = screen;
     this.navigation = navigation;
--- src/app/map/navigation.service.ts
+++ src/app/map/navigation.service.ts
@@ -5,9 +5,18 @@ export class NavigationService {
   scale: number;
   left: number;
   top: number;
+  x: number;
+  y: number;
+  deltaX:number;
+  deltaY:number;
+  mouseDown : boolean;
   constructor() {
     this.left = 1;
     this.top = 2;
     this.scale = 0.5;
+
+    this.x = 0;
+    this.y = 0;
+    this.mouseDown = false;
   }
 }
