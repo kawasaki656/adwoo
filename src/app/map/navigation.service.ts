01bdada758b71db9651fe503835e967173768103
kawasaki646 <kawasaki646@gmail.com>
2017-10-18 11:18:14 +0300

    refactoring

--- src/app/map/navigation.service.ts
+++ src/app/map/navigation.service.ts
@@ -2,18 +2,12 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class NavigationService {
-  leftNav: any;
   scale: number;
   left: number;
   top: number;
   constructor() {
-    this.leftNav = document.getElementsByClassName("leftNav");
-    //this.leftNav.addEventListener("click",()=>{console.log("click left navv")})
     this.left = 1;
     this.top = 2;
-  }
-
-  addEventHandlers(): void {
-    this.leftNav = document.getElementsByClassName("leftNav")[0];
+    this.scale = 0.5;
   }
 }
