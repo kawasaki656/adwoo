1bf7a2e6bf9bbd3376df2a40985fda1c5052707e
kawasaki646 <kawasaki646@gmail.com>
2018-03-02 15:05:41 +0300

    complete registration layout without icons

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -7,8 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  checkAgreements: any;
-  constructor() { }
+  checkAgreements: boolean;
+  login: string;
+  step: number;
+  constructor() {
+    this.step = 0;
+    this.checkAgreements = false;
+  }
+
+  changeInput(arg) {
+    if(this.login !== undefined && this.login !== '') {
+      switch(arg) {
+        case 'login': this.step = 1; break;
+        case 'password': this.step = 2; break;
+        case 'rules': this.checkAgreements ? this.step = 2 : this.step = 3;
+      }
+    } else {
+      switch(arg) {
+        case 'login': this.step = 0; break;
+        case 'password': this.step = 1; break;
+      }
+    }
+    console.log(this.step)
+  }
 
   ngOnInit() {
   }
