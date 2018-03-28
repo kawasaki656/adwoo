fc8944bb10ec306b3f657ee5e508f0ed6f6ab76b
kawasaki646 <kawasaki646@gmail.com>
2018-03-28 19:18:34 +0300

    define blocks

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  account: boolean;
   constructor() { }
 
   ngOnInit() {
+    this.account = true;
   }
 
 }
